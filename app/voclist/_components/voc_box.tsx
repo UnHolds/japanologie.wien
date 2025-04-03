c8d80033c8d7a6bdb444bca6aa2175d2dfba48e4
UnHold (Alex) <alex@hold.re>
2025-04-03 13:26:49 +0200

    added blur for voc

--- app/voclist/_components/voc_box.tsx
+++ app/voclist/_components/voc_box.tsx
@@ -10,15 +10,24 @@ export interface Voc {
 
 interface Props {
     voc: Voc
+    hideKanaKanji: boolean,
+    hideMeaning: boolean
 }
 
 
-export default function VocBox({ voc }: Props) {
+export default function VocBox({ voc, hideKanaKanji, hideMeaning }: Props) {
+
     return (
     <div className="grid grid-cols-3 p-2 relative">
-        <div>{voc.kana}</div>
-        <div className="text-center">{voc.kanji}</div>
-        <div className="text-right">{voc.meaning}</div>
+        <div className="group">
+            <div className={`w-fit ${hideKanaKanji ? "blur-lg bg-gray-100 group-hover:blur-none group-hover:bg-inherit" : ""}`}>{voc.kana}</div>
+        </div>
+        <div className="flex justify-center group">
+            <div className={`w-fit ${hideKanaKanji ? "blur-lg bg-gray-100 group-hover:blur-none group-hover:bg-inherit" : ""}`}>{voc.kanji}</div>
+        </div>
+        <div className="flex justify-end group">
+            <div className={`w-fit ${hideMeaning ? "blur-lg bg-gray-100 group-hover:blur-none group-hover:bg-inherit" : ""}`}>{voc.meaning}</div>
+        </div>
     </div>
     )
 }
--- app/voclist/page.tsx
+++ app/voclist/page.tsx
@@ -1,11 +1,26 @@
+"use client"
+
+import { useState } from "react";
 import vocs from "../assets/voc.json";
 import VocBox from "./_components/voc_box";
 
 export default function VocList() {
     let curr_lesson = -1;
+    const [hideKanaKanji, setHideKanaKanji] = useState(false);
+    const [hideMeaning, setHideMeaning] = useState(false);
     return (
         <div className="my-10">
             <div className="text-center text-6xl font-bold mb-5">Vokabel-Liste</div>
+            <div className="flex justify-center gap-6 mb-5">
+                <div className="flex items-center gap-2">
+                    <input type="checkbox" name="hide_kana" className="w-5 h-5" checked={hideKanaKanji} onChange={() => {setHideKanaKanji(!hideKanaKanji)}}/>
+                    <label htmlFor="hide_kana" className="text-xl">Hide Kana / Kanji</label>
+                </div>
+                <div className="flex items-center gap-2">
+                    <input type="checkbox" name="hide_kanji" className="w-5 h-5" checked={hideMeaning} onChange={() => {setHideMeaning(!hideMeaning)}}/>
+                    <label htmlFor="hide_kana" className="text-xl">Hide Meaning</label>
+                </div>
+            </div>
             <div className="w-full flex justify-center">
                 <div className="h-full flex flex-col gap-2 xl:w-1/3 lg:w-2/3">
                     {
@@ -16,7 +31,7 @@ export default function VocList() {
                             elems.push(<div key={`${i}-l`} className="text-center font-bold text-4xl">{curr_lesson} 課</div>);
                             elems.push(<div  key={`${i}-lb`} className="border-t-1"/>);
                         }
-                        elems.push(<VocBox key={`${i}-voc`} voc={v}/>);
+                        elems.push(<VocBox key={`${i}-voc`} voc={v} hideKanaKanji={hideKanaKanji} hideMeaning={hideMeaning}/>);
                         elems.push(<div  key={`${i}-b`} className="border-t-1"/>);
                         return elems;
                     })
